d3ebe7d2c2b17cefe4ec64f7eefd20c309ffce8d
thomas dubiel <github email address>
2020-10-02 22:43:04 -0500

    start studentsContainer and App.js files

---
+++ frontend/kids_bank_client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StudentsContainer from './StudentsContainer'
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 align="center">Kids Bank</h1>
+      <StudentsContainer />
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/kids_bank_client/src/components/StudentsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StudentsContainer extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default StudentsContainer;
