5c819f28b052d88931ce05dd667c5b7c7c0a289c
thomas dubiel <github email address>
2020-10-03 22:01:39 -0500

    studentsForm first pass

--- frontend/kids_bank_client/src/components/StudentsForm.js
+++ frontend/kids_bank_client/src/components/StudentsForm.js
@@ -4,9 +4,25 @@ class StudentsForm extends Component {
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
+        <label>Name</label>
+        <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
+        <br />
 
-      </form> 
+        <label>Email</label>
+        <input type="email" value={this.state.email} onChange={this.handleChange} name="email" />
+        <br />
+
+        <label>Allowance<label>
+        <input type="text" value={this.state.allowance} onChange={this.handleChange} name="allowance" />
+        <br />
+
+        <label>Balance</label>
+        <input type="text" value={this.state.balance} onChange={this.handleChange} name="balance" />
+        <br />
+
+        <input type="submit" value="Create New Student"
+      </form>
     );
   }
 
