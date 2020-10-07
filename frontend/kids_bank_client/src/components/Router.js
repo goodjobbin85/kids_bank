c6326e65b21a0ebe84e4d68f2ea873b04e5fac47
thomas dubiel <github email address>
2020-10-07 10:33:51 -0500

    create routed to files and import said files to routes file

--- frontend/kids_bank_client/src/components/Router.js
+++ frontend/kids_bank_client/src/components/Router.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+import Home from './Home';
+import About from './About'
+import StudentsList from './StudentsList'; 
 
 const Router = () => (
   return (
     <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/about' component={About} />
-      <Route exact path='/students' component={StudentsList} /> 
+      <Route exact path='/students' component={StudentsList} />
     </Switch>
   )
 
