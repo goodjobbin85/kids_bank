be22d8d46717832e931aef8167f2429863c4ca1b
thomas dubiel <github email address>
2020-10-09 14:06:29 -0500

    update about.js component

--- frontend/kids_bank_client/src/components/About.js
+++ frontend/kids_bank_client/src/components/About.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are a service that helps parents keep track of their children's chores,
+        helping them to become responsible with their finances from an early age.
+      </p>
+    </div>
   );
 }
 
