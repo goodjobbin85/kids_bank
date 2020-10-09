8e94451a06267af1fd40a4f7a698ad62374753c4
thomas dubiel <github email address>
2020-10-09 17:04:31 -0500

    create nested resources for tasks and students

--- backend/kids_bank/config/routes.rb
+++ backend/kids_bank/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :students
+
+  resources :students do
+    resources :tasks
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
